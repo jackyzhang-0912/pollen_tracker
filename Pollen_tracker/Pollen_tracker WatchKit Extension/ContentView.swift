b825566e0e13d88ffb648d2d5cb4e3e23ad791e5
Jacky Zhang <yzhang69@email.wm.edu>
2022-04-26 16:50:11 -0400

    initialize repo

---
+++ Pollen_tracker/Pollen_tracker WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pollen_tracker WatchKit Extension
+//
+//  Created by Skywalker on 2022/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pollen_tracker/Pollen_tracker WatchKit Extension/Pollen_trackerApp.swift
@@ -0,0 +1,19 @@
+//
+//  Pollen_trackerApp.swift
+//  Pollen_tracker WatchKit Extension
+//
+//  Created by Skywalker on 2022/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Pollen_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
